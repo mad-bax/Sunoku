f6a4d7fbbb1c0b9bd8ceb406f5fc0d80d5bbe2c4
mad_bax <madbax@proton.me>
2025-11-23 13:56:45 -0500

    updates

--- src_c/prog.c
+++ src_c/prog.c
@@ -1,58 +1,14 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-static int** memory;
-static int* cells;
-static int max_val;
-static int segment_size;
-static int size;
+typedef uint64_t u64;
 
-#define G(r, c) [((size*r)+c)]
+static u64 x, *C, H, S, *M;
+
+#define G(i, j) [((S*i)+j)]
 #define pf printf
+#define rt return
 
-void ib(char* fname){FILE*f=fopen(fname,"r");if(fscanf(f, "%d",&size)!=1)goto id;max_val=size;segment_size=0;if(cells!=NULL)free(cells);cells=(int*)malloc(sizeof(int)*(size*size));for(int i=0;;i++){if(i==(size*size))break;if(fscanf(f,"%d",&cells[i])!=1){break;}if(i==0)continue;if(size/i==i&&size%i==0)segment_size=i;}id:fclose(f);return;}void pl(int o){pf(" ");for(int j=1;j<o;j++){pf("-");}pf("\n");}void pb(){int m=floor(log10(size))+1;int o=(size*m)+(2*segment_size)+3+(size-1);for(int i=0;i<size;i++){if(i%segment_size==0)pl(o);for(int j=0;j<size;j++){if(j%segment_size==0){pf(" |");}int v=cells G(i,j);int l=m-1;if(v!=0)l=(m-floor(log10(v)))-1;for(int k=0;k<l;k++){pf(" ");}pf(" %d", v);}pf(" |\n");}pl(o);}int check(int v,int r,int c){for(int i=0;i<size;i++){if(cells G(r,i)==v)return 0;if(cells G(i,c)==v)return 0;}int y=(r/segment_size)*segment_size;int f=(c/segment_size)*segment_size;for(int p=y;p<y+segment_size;p++){for(int g=f;g<f+segment_size;g++){if(g==c&&p==r)continue;if(cells G(p,g)==v)return 0;}}return 1;}int n(int r,int c){if(r>=size)return 1;if(cells G(r,c)!=0){if(c+1>=size)return n(r+1,0);else return n(r,c+1);}for(int v=1;v<=max_val;v++){if(check(v,r,c)!=0){cells G(r, c) = v;if(c+1>=size)if(n(r+1,0)== 0) cells G(r,c)=0;else break;else if(n(r,c+1)==0) cells G(r,c)=0;else break;}}return cells G(r,c)!=0;}
-
-int mem_one(int r, int c) {
-    int ret = 0;
-
-    for (int i=0;i<size;i++) {
-        ret += memory G(r, c) [i];
-    }
-
-    return ret;
-}
-
-int pop(int r, int c) {
-    for(int i=0;i<size;i++){
-        int t = memory G(r, c)[i];
-        if (t) return t;
-    }
-    return -1;
-}
-
-void clear(int r, int c) {
-    for (int i=0;i<size;i++) {
-        memory G(r, c)[i] = 0;
-    }
-}
-
-void mem_remove(int v, int r, int c, int ri, int re, int ci, int ce) {
-    for (int i=0;i<size;i++) {
-        memory G(r, i)[v] = 0;
-        memory G(i, c)[v] = 0;
-    }
-
-    for (int i=ri;i<re;i++) {
-        for(int j=ci;j<ce;j++) {
-            memory G(i, j)[v] = 0;
-        }
-    }
-}
-
-int b() {
-
-    return n(0, 0);
-}
-
-int main(int c,char** a){for(int i=1;i<c;i++){ib(a[i]);};b();pb();return 0;}
+void ib(char*n){FILE*f=fopen(n,"r");if(fscanf(f, "%ld",&S)!=1)goto id;x=S;H=0;if(C!=NULL)free(C);C=(u64*)malloc(sizeof(u64)*(S*S));for(u64 i=0;;i++){if(i==(S*S))break;if(fscanf(f,"%ld",&C[i])!=1)break;if(i==0)continue;if(S/i==i&&S%i==0)H=i;}id:fclose(f);rt;}void pl(u64 o){pf(" ");for(u64 j=1;j<o;j++)pf("-");pf("\n");}void pb(){u64 m=floor(log10(S))+1;u64 o=(S*m)+(2*H)+(S+2);for(u64 i=0;i<S;i++){if(i%H==0)pl(o);for(u64 j=0;j<S;j++){if(j%H==0)pf(" |");u64 v=C G(i,j);u64 l=m-1;if(v!=0)l=(m-floor(log10(v)))-1;for(u64 k=0;k<l;k++){pf(" ");}pf(" %ld", v);}pf(" |\n");}pl(o);}u64 ch(u64 v,u64 r,u64 c){for(u64 i=0;i<S;i++){if(C G(r,i)==v)rt 0;if(C G(i,c)==v)rt 0;}u64 y=(r/H)*H;u64 f=(c/H)*H;for(u64 p=y;p<y+H;p++)for(u64 g=f;g<f+H;g++){if(g==c&&p==r)continue;if(C G(p,g)==v)rt 0;}rt 1;}u64 n(u64 r,u64 c){if(r>=S)rt 1;if(C G(r,c)!=0){if(c+1>=S)rt n(r+1,0);else rt n(r,c+1);}for(u64 v=1;v<=x;v++){if(ch(v,r,c)!=0){C G(r,c)=v;if(c+1>=S)if(n(r+1,0)== 0) C G(r,c)=0;else break;else if(n(r,c+1)==0) C G(r,c)=0;else break;}}rt C G(r,c)!=0;}void F(u64 v,u64 r,u64 c,u64 rs,u64 re,u64 zs,u64 cz) {u64 k=~(1<<v);for (int i=0;i<S;i++){M G(r, i)&=k;M G(i,c)&=k;}for(int i=rs;i<re;i++)for(int j=zs;j<cz;j++){M G(i,j)&=k;}}int Y(u64 v,u64 r,u64 c,u64 rs,u64 re,u64 zs,u64 cz){if((M G(r,c)&(1<<v))==0)rt 0;for(int i=rs;i<re;i++)for(int j=zs;j<cz;j++){if(c==j&&r==i)continue;if((M G(i,j)&(1<<v))!=0)rt 0;}C G(r,c)=v;M G(r,c)=0;F(v,r,c,rs,re,zs,cz);rt 1;}int V(u64 v,u64 r,u64 c,u64 rs,u64 re,u64 zs,u64 cz){if((M G(r,c)&(1<<v))==0)rt 0;int L=0,T=0,P=0,W=0;for(int i=rs;i<re;i++)for(int j=zs;j<cz;j++){if(r==i&&c==j)continue;if((M G(i,j)&(1<<v))!=0){if(i==r)P=1;else if(j==c)T=1;else W=1;}}if(W||(T&&P))rt 0;if(T)for(int i=0;i<S;i++){if(i>=rs&&i<re)continue;if((M G(i,c)&(1<<v))!=0){M G(i,c)&=~(1<<v);L=1;}}else if(P)for(int i=0;i<S;i++){if(i>=zs&&i<cz)continue;if((M G(r,i)&(1<<v))!=0){M G(r,i)&=~(1<<v);L=1;}}rt L;}u64 b(){if(M!=NULL)free(M);M=(u64*)malloc(sizeof(u64)*(S*S));for(u64 i=0;i<S;i++){for(u64 j=0;j<S;j++){M G(i,j)=0;if(C G(i,j)!=0)continue;for(u64 k=1;k<=x;k++)if(ch(k,i,j))M G(i,j)|=1<<k;}}int D=1;while(D){D-=1;for(u64 v=1;v<=x;v++){for(u64 r=0;r<S;r++){u64 bsr=r/H;u64 rs=bsr*H;u64 re=rs+H;for(u64 c=0;c<S;c++){if(C G(r,c)!=0)continue;u64 B=M G(r,c);u64 bsc=c/H;u64 zs=bsc*H;u64 cz=zs+H;if((B&(B-1))==0){u64 Z = 0;for(;B>1;B>>=1,Z++){}C G(r,c)=Z;M G(r,c)=0;F(Z,r,c,rs,re,zs,cz);D=1;continue;}if(Y(v,r,c,rs,re,zs,cz)){D=1;continue;}if(V(v,r,c,rs,re,zs,cz))D=1;}}}}rt n(0,0);}int main(int c,char** a){for(u64 i=1;i<c;i++)ib(a[i]);b();pb();rt 0;}
---
+++ src_c/prog_orig.c
@@ -0,0 +1,215 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef uint64_t u64;
+
+static u64* M;
+static u64* C;
+static u64 x;
+static u64 H;
+static u64 S;
+
+#define G(r, c) [((S*r)+c)]
+#define pf printf
+#define rt return
+
+void ib(char*n){
+    FILE*f=fopen(n,"r");
+
+    if(fscanf(f, "%ld",&S)!=1)
+        goto id;
+    x=S;
+    H=0;
+
+    if(C!=NULL)free(C);
+
+    C=(u64*)malloc(sizeof(u64)*(S*S));
+
+    for(u64 i=0;;i++){
+        if(i==(S*S))break;
+        if(fscanf(f,"%ld",&C[i])!=1)break;
+        if(i==0)continue;
+        if(S/i==i&&S%i==0)H=i;}
+id:
+    fclose(f);
+    rt;
+}
+
+void pl(u64 o){
+    pf(" ");
+    for(u64 j=1;j<o;j++)pf("-");
+    pf("\n");
+}
+
+void pb(){
+    u64 m=floor(log10(S))+1;
+    u64 o=(S*m)+(2*H)+(S+2);
+    for(u64 i=0;i<S;i++){
+        if(i%H==0)pl(o);
+        for(u64 j=0;j<S;j++){
+            if(j%H==0)pf(" |");
+            u64 v=C G(i,j);
+            u64 l=m-1;
+            if(v!=0)l=(m-floor(log10(v)))-1;
+            for(u64 k=0;k<l;k++){
+                pf(" ");
+            }
+            pf(" %ld", v);
+        }
+        pf(" |\n");
+    }
+    pl(o);
+}
+
+u64 ch(u64 v,u64 r,u64 c){
+    for(u64 i=0;i<S;i++){
+        if(C G(r,i)==v)rt 0;
+        if(C G(i,c)==v)rt 0;
+    }
+    u64 y=(r/H)*H;
+    u64 f=(c/H)*H;
+    for(u64 p=y;p<y+H;p++)
+        for(u64 g=f;g<f+H;g++){
+            if(g==c&&p==r)continue;
+            if(C G(p,g)==v)rt 0;
+        }
+    rt 1;
+}
+
+u64 n(u64 r,u64 c){
+    if(r>=S)rt 1;
+    if(C G(r,c)!=0){
+        if(c+1>=S)rt n(r+1,0);
+        else rt n(r,c+1);
+    }
+    for(u64 v=1;v<=x;v++){
+        if(ch(v,r,c)!=0){
+            C G(r,c)=v;
+            if(c+1>=S)
+                if(n(r+1,0)== 0) C G(r,c)=0;
+                else break;
+            else if(n(r,c+1)==0) C G(r,c)=0;
+            else break;
+        }
+    }
+
+    rt C G(r,c)!=0;
+}
+
+void F(u64 v,u64 r,u64 c,u64 rs,u64 re,u64 zs,u64 cz) {
+    u64 k=~(1<<v);
+    for (int i=0;i<S;i++){
+        M G(r, i)&=k;M G(i,c)&=k;
+    }
+    for(int i=rs;i<re;i++)
+        for(int j=zs;j<cz;j++){
+            M G(i,j)&=k;
+        }
+}
+
+int Y(u64 v,u64 r,u64 c,u64 rs,u64 re,u64 zs,u64 cz){
+    if((M G(r,c)&(1<<v))==0)rt 0;
+
+    for(int i=rs;i<re;i++)
+        for(int j=zs;j<cz;j++){
+            if(c==j&&r==i)continue;
+            if((M G(i,j)&(1<<v))!=0)rt 0;
+        }
+
+    C G(r,c)=v;
+    M G(r,c)=0;
+    F(v,r,c,rs,re,zs,cz);
+    rt 1;
+}
+
+int V(u64 v,u64 r,u64 c,u64 rs,u64 re,u64 zs,u64 cz){
+    if((M G(r,c)&(1<<v))==0)rt 0;
+
+    int L=0,T=0,P=0,W=0;
+    for(int i=rs;i<re;i++)
+        for(int j=zs;j<cz;j++){
+            if(r==i&&c==j)continue;
+            if((M G(i,j)&(1<<v))!=0){
+                if(i==r)P=1;
+                else if(j==c)T=1;
+                else W=1;
+            }
+        }
+
+    if(W||(T&&P))rt 0;
+    if(T)
+        for(int i=0;i<S;i++){
+            if(i>=rs&&i<re)continue;
+            if((M G(i,c)&(1<<v))!=0){
+                M G(i,c)&=~(1<<v);
+                L=1;
+            }
+        }
+    else if(P)
+        for(int i=0;i<S;i++){
+            if(i>=zs&&i<cz)continue;
+            if((M G(r,i)&(1<<v))!=0){
+                M G(r,i)&=~(1<<v);
+                L=1;
+            }
+        }
+
+    rt L;
+}
+
+u64 b(){
+    if(M!=NULL)free(M);
+    M=(u64*)malloc(sizeof(u64)*(S*S));
+    for(u64 i=0;i<S;i++){
+        for(u64 j=0;j<S;j++){
+            M G(i,j)=0;
+            if(C G(i,j)!=0)continue;
+            for(u64 k=1;k<=x;k++)
+                if(ch(k,i,j))
+                    M G(i,j)|=1<<k;
+        }
+    }
+
+    int D=1;
+    while(D){
+        D-=1;
+        for(u64 v=1;v<=x;v++){
+            for(u64 r=0;r<S;r++){
+                u64 bsr=r/H;
+                u64 rs=bsr*H;
+                u64 re=rs+H;
+                for(u64 c=0;c<S;c++){
+                    if(C G(r,c)!=0)continue;
+                    u64 B=M G(r,c);
+                    u64 bsc=c/H;
+                    u64 zs=bsc*H;
+                    u64 cz=zs+H;
+                    if((B&(B-1))==0){
+                        u64 Z = 0;
+                        for(;B>1;B>>=1,Z++){}
+                        C G(r,c)=Z;
+                        M G(r,c)=0;F(Z,r,c,rs,re,zs,cz);
+                        D=1;
+                        continue;
+                    }
+                    if(Y(v,r,c,rs,re,zs,cz)){
+                        D=1;
+                        continue;
+                    }
+                    if(V(v,r,c,rs,re,zs,cz)) D=1;
+                }
+            }
+        }
+    }
+    rt n(0,0);
+}
+
+int main(int c,char** a){
+    for(u64 i=1;i<c;i++)
+        ib(a[i]);
+    b();
+    pb();
+    rt 0;
+}
